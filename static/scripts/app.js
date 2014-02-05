562a1a28744dac2f686f56969aff0b40d027d029
Wil Linssen <wil@linssen.me>
2014-02-05 16:38:28 +0000

    Ditch select2 for straight up angular (that doesn't do anything)

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,135 +1,121 @@
 /*global angular*/
 'use strict';
 
-var btpApp = angular.module('btpApp', [
-    'ngRoute',
-    'ngResource',
-
-    'ui.select2',
-
-    'btpControllers',
-    'btpServices'
-]);
-var btpControllers = angular.module('btpControllers', []);
-var btpServices = angular.module('btpServices', ['ngResource']);
 var PREDATOR = '16751';
 
-btpApp.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider
-            .when('/versus/:id/:title', {
-                templateUrl: 'static/scripts/templates/versus.html',
-                controller: 'VersusCtrl'
-            })
-            .when('/', {
-                templateUrl: 'static/scripts/templates/search.html',
-                controller: 'SearchCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    }]);
+angular.module('btpApp', [
+    'ngRoute',
+    'ngResource',
+    'btp.controllers',
+    'btp.services'
+]).
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/versus/:id/:title', {
+            templateUrl: 'static/scripts/templates/versus.html',
+            controller: 'VersusCtrl'
+        })
+        .when('/', {
+            templateUrl: 'static/scripts/templates/search.html',
+            controller: 'SearchCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
 
-btpApp.filter('urlize', function () {
-    return function (str) {
-        str = str
-            .replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, '')
-            .replace(/\s{2,}/g, ' ')
-            .replace(/\s/g, '-')
-            .toLowerCase();
-        return window.encodeURIComponent(str);
-    };
-});
-
-
-btpControllers.controller('SearchCtrl', ['$scope', 'Film',
-    function ($scope, Film) {
-        $scope.select2 = {
-            minimumInputLength: 2,
-            ajax: {
-                url: "http://api.rottentomatoes.com/api/public/v1.0/movies.json",
-                dataType: 'jsonp',
-                quietMillis: 100,
-                data: function (term) {
-                    return {
-                        q: term,
-                        page_limit: 10,
-                        page: 1,
-                        apikey: '6ynntf95p6h4pb8df3v73r7q'
-                    };
-                },
-                results: function (data) {
-                    return {results: data.movies};
-                }
-            },
-            id: function (film) { return film.id; },
-            formatResult: function (film) {
-                return '<img src=":thumb" height="40" alt=":title"> :title (:year)'
-                    .replace(/\:title/g, film.title)
-                    .replace(/\:thumb/g, film.posters.thumbnail)
-                    .replace(/\:year/g, film.year);
-            },
-            formatSelection: function (film) { return film.title; }
+angular.module('btp.filters', [])
+    .filter('urlize', function () {
+        return function (str) {
+            str = str
+                .replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+                .replace(/\s{2,}/g, ' ')
+                .replace(/\s/g, '-')
+                .toLowerCase();
+            return window.encodeURIComponent(str);
         };
-    }]);
+    });
 
-btpControllers.controller('VersusCtrl', ['$scope', '$routeParams', '$location', '$q', '$window', 'Film',
-    function ($scope, $routeParams, $location, $q, $window, Film) {
-        $q.all([
-            Film.get({id: PREDATOR}),
-            Film.get({id: $routeParams.id})
-        ]).then(function (result) {
-            $scope.films = [result[0], result[1]];
-        });
 
-        $scope.now = new Date();
-        $scope.shareUrl = window.encodeURIComponent(
-            'http://betterthanpredator.com/#' + $location.path()
-        );
+angular.module('btp.controllers', [])
+    .controller('SearchCtrl', ['$scope', 'Film',
+        function ($scope, Film) {
+            var watching;
+            watching = true;
+            $scope.films = [];
+            $scope.title = '';
+            $scope.$watch('title', function (newValue, oldValue) {
+                if (newValue.length < 3) { return; }
+                $scope.films = Film.query({q: newValue});
+            });
+        }]
+    )
+    .controller('VersusCtrl', ['$scope', '$routeParams', '$location', '$q', '$window', 'Film',
+        function ($scope, $routeParams, $location, $q, $window, Film) {
+            $q.all([
+                Film.get({id: PREDATOR}),
+                Film.get({id: $routeParams.id})
+            ]).then(function (result) {
+                $scope.films = [result[0], result[1]];
+            });
 
-        $scope.$on('$routeChangeSuccess', function () {
-            $window._gaq.push(['_trackPageview', $location.path()]);
-            $window._gaq.push([
-                '_trackEvent',
-                'Film',
-                'Compare',
-                $routeParams.id + ' - ' + $routeParams.title
-            ]);
+            $scope.now = new Date();
+            $scope.shareUrl = window.encodeURIComponent(
+                'http://betterthanpredator.com/#' + $location.path()
+            );
 
-        });
-    }]);
+            $scope.$on('$routeChangeSuccess', function () {
+                $window._gaq.push(['_trackPageview', $location.path()]);
+                $window._gaq.push([
+                    '_trackEvent',
+                    'Film',
+                    'Compare',
+                    $routeParams.id + ' - ' + $routeParams.title
+                ]);
 
-btpServices.factory('Film', ['$resource',
-    function ($resource) {
-        return $resource('http://api.rottentomatoes.com/api/public/v1.0/movies/:id.json',
-            {
+            });
+        }]
+    );
+
+angular.module('btp.services', [])
+    .factory('Film', ['$resource',
+        function ($resource) {
+            var defaultParams;
+            defaultParams = {
                 apikey: '6ynntf95p6h4pb8df3v73r7q',
                 callback: 'JSON_CALLBACK'
-            },
-            {
-                query: {
-                    method: 'JSONP',
-                    isArray: true,
-                    id: '',
-                    transformResponse: function (data) {
-                        return data.movies;
-                    }
-                },
-                get: {
-                    method: 'JSONP',
-                    isArray: false,
-                    id: '@id',
-                    transformResponse: function (data) {
-                        var date;
-                        data.ratings.combined = (data.ratings.critics_score + data.ratings.audience_score) / 2;
-                        data.ratings.combined = Math.round(data.ratings.combined) / 10;
-                        date = data.release_dates.theater || data.release_dates.dvd || null;
-                        if (data.release_dates.computed) {
-                            data.release_dates.computed = new Date(date.split('-').reverse());
+            };
+            return $resource('http://api.rottentomatoes.com/api/public/v1.0/movies/:id.json',
+                defaultParams,
+                {
+                    query: {
+                        method: 'JSONP',
+                        isArray: true,
+                        params: {
+                            q: 'honey',
+                            page_limit: 10,
+                            page: 1
+                        },
+                        transformResponse: function (data) {
+                            console.log(data);
+                            return data.movies;
                         }
+                    },
+                    get: {
+                        method: 'JSONP',
+                        isArray: false,
+                        transformResponse: function (data) {
+                            var date;
+                            data.ratings.combined = (data.ratings.critics_score + data.ratings.audience_score) / 2;
+                            data.ratings.combined = Math.round(data.ratings.combined) / 10;
+                            date = data.release_dates.theater || data.release_dates.dvd || null;
+                            if (data.release_dates.computed) {
+                                data.release_dates.computed = new Date(date.split('-').reverse());
+                            }
 
-                        return data;
+                            return data;
+                        }
                     }
                 }
-            });
-    }]);
+            );
+        }
+    ]);
