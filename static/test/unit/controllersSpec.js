64d1cf3afed14cff82a82964add83facf9f780d3
Wil Linssen <wil@linssen.me>
2014-02-06 12:51:29 +0000

    Starting a test for the search ctrl

--- static/test/unit/controllersSpec.js
+++ static/test/unit/controllersSpec.js
@@ -1,12 +1,35 @@
 'use strict';
 
 describe('BTP controllers', function () {
-    beforeEach(module('btpApp'));
+    beforeEach(module('ngResource'));
+    beforeEach(module('btp.controllers'));
+    beforeEach(module('btp.services'));
 
     describe('SearchCtrl', function () {
+        var $httpBackend, film, scope;
 
-        it('should return 10 via xhr films with a proper search term', function () {
-            expect(true).toEqual(true);
-        });
+        beforeEach(inject(function (_$httpBackend_, $injector, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('http://api.rottentomatoes.com/api/public/v1.0/movies.json')
+                .respond([
+                    {id: 12233, title: 'Something'},
+                    {id: 48733, title: 'Else'}
+                ]);
+            scope = $rootScope.$new();
+            film = $injector.get('Film');
+            $controller('SearchCtrl', {
+                $scope: scope,
+                Film: film
+            });
+        }));
+
+        it('should return 10 via xhr films with a proper search term', inject(function () {
+            expect(scope.films).toEqual([]);
+            // $httpBackend.flush();
+            // expect(scope.films).toEqual([
+            //     {id: 12233, title: 'Something'},
+            //     {id: 48733, title: 'Else'}
+            // ]);
+        }));
     });
 });
