688c457453bc60542f3cfa2720f9c2b5a8cef308
Wil Linssen <wil@linssen.me>
2014-02-08 13:16:31 +0000

    Keyboard controls for autocomplete
    - Better styles too

--- static/scripts/directives.js
+++ static/scripts/directives.js
@@ -1,15 +1,53 @@
 'use strict';
 
-angular.module('btp.directives', [])
-    .directive('ngFilmAutocomplete', function (Film) {
+angular.module('btp.directives', ['btp.filters'])
+    .directive('ngFilmAutocomplete', function ($location, $filter, Film) {
         return {
             restrict: 'A',
-            templateUrl: '../static/scripts/templates/film-auto-complete.tpl.html',
-            link: function (scope) {
-                scope.$watch('title', _.debounce(function (newValue) {
+            templateUrl: '../static/scripts/templates/film-autocomplete.tpl.html',
+            link: function (scope, element) {
+                var autocomplete, choose, keys, keyup, nav;
+
+                keys = {up: 38, down: 40, enter: 13};
+                scope.activeIndex = 0;
+
+                autocomplete = function (newValue) {
                     if (newValue && newValue.length < 3) { return; }
                     scope.films = Film.query({q: newValue});
-                }, 100));
+                };
+                keyup = function (ev, element) {
+                    switch (ev.which) {
+                    case keys.up:
+                    case keys.down:
+                        ev.preventDefault();
+                        nav(ev.which === keys.down ? 1 : -1);
+                        break
+                    case keys.enter:
+                        ev.preventDefault();
+                        choose();
+                    };
+                };
+                nav = function (direction) {
+                    var numFilms;
+                    numFilms = scope.films.length;
+                    scope.activeIndex += direction;
+                    if (scope.activeIndex < 0) { scope.activeIndex = 0; return; }
+                    if (scope.activeIndex >= numFilms) { scope.activeIndex = numFilms - 1; return; }
+                    scope.$apply();
+                };
+                choose = function () {
+                    var film, path;
+                    film = scope.films[scope.activeIndex];
+                    path = '/versus/:id/:title'
+                        .replace(':id', film.id)
+                        .replace(':title', $filter('urlize')(film.title));
+                    $location.path(path);
+                    scope.$apply();
+                };
+
+                // Bindings etc.
+                scope.$watch('title', _.debounce(autocomplete, 100));
+                element.bind('keyup', keyup);
             }
         };
     }
