c664c1643c373a887de44aa66c2671addc16b86e
Wil Linssen <wil@linssen.me>
2014-02-10 08:37:04 +0000

    Don't XHR for templates, they're compiled

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -13,11 +13,11 @@ angular.module('btpApp', [
 ])
 .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/versus/:id/:title', {
-        templateUrl: 'static/scripts/templates/versus.tpl.html',
+        templateUrl: '../static/scripts/templates/versus.tpl.html',
         controller: 'VersusCtrl'
     })
     .when('/', {
-        templateUrl: 'static/scripts/templates/search.tpl.html',
+        templateUrl: '../static/scripts/templates/search.tpl.html',
         controller: 'SearchCtrl'
     })
     .otherwise({
