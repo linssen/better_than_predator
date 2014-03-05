165a6e1bab6b1a0525b21e37f78d32e58d0bd5fa
Wil Linssen <wil@linssen.me>
2014-03-05 10:19:33 +0000

    I MUST mock the API

--- static/test/integration/scenarios.js
+++ static/test/integration/scenarios.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('BTP pages', function () {
-    var API_BASE, base, expectedFilms, expectedVersusURL, ptor;
+    var base, expectedFilms, expectedVersusURL, ptor;
 
     base = 'http://localhost:9001/#';
     ptor = protractor.getInstance();
@@ -26,33 +26,34 @@ describe('BTP pages', function () {
         query = element(by.model('query'));
         films = protractor.By.repeater('f in films');
 
-        query.sendKeys('honey')
+        query.sendKeys('honey');
 
         ptor.wait(function () {
             return ptor.isElementPresent(films);
         }).then(function () {
-            var expectedTitle, film0, film4;
+            var expectedTitle, film0, film1, film1Position;
 
             expectedTitle = ':title (:year)'
                 .replace(':title', expectedFilms().movies[0].title)
                 .replace(':year', expectedFilms().movies[0].year);
 
+            film1Position = 3;
             film0 = element(films.row(0).column('{{f.title}}'));
-            film4 = element(films.row(4).column('{{f.title}}'));
+            film1 = element(films.row(film1Position).column('{{f.title}}'));
 
             // We've limited the results to 10 and we should get that many
             expect(element.all(films).count()).toEqual(10);
             // The second result (FOR NOW) is HISTK
-            expect(film4.getText()).toEqual(expectedTitle);
-            // Move down four
-            query.sendKeys(protractor.Key.DOWN);
-            query.sendKeys(protractor.Key.DOWN);
-            query.sendKeys(protractor.Key.DOWN);
-            query.sendKeys(protractor.Key.DOWN);
+            expect(film1.getText()).toEqual(expectedTitle);
+            // Move down to film1
+            for (var i = 0; i < film1Position; i += 1) {
+                query.sendKeys(protractor.Key.DOWN);
+            }
+
             // Make sure the first film isn't active
             expect(film0.getAttribute('class')).not.toContain('active');
             // Make sure HISTK is
-            expect(film4.getAttribute('class')).toContain('active');
+            expect(film1.getAttribute('class')).toContain('active');
             // Select a flm with the enter key
             query.sendKeys(protractor.Key.ENTER);
             ptor.waitForAngular();
