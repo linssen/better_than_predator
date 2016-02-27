5479c542909d3e6f38f77ea4b257ef8ef5db1fbb
Wil Linssen <wil@linssen.me>
2016-02-27 23:54:36 +0000

    Use hash history for permalinks

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, Link, hashHistory } from 'react-router'
 
 import Search from './modules/Search'
 import Results from './modules/Results'
@@ -9,7 +9,7 @@ import Results from './modules/Results'
 window.React = React;
 
 ReactDOM.render((
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={Search}/>
     <Route path="/versus" component={Results}/>
   </Router>
