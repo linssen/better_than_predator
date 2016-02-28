f4d8368e2712d8a195a60074f1e99c701b3bbc50
Wil Linssen <wil@linssen.me>
2016-02-28 15:33:13 +0000

    String utils begun

---
+++ static/scripts/utils/StringUtils.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export function slugify(str) {
+    str = str
+        .replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+        .replace(/\s{2,}/g, ' ')
+        .replace(/\s/g, '-')
+        .toLowerCase();
+    return window.encodeURIComponent(str);
+}
