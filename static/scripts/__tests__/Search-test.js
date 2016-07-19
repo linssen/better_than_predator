45ceb1ae7baa5f1760f02cea1f8292e766acff8f
Wil Linssen <wil@linssen.me>
2016-07-19 19:30:37 +0100

    Test for keys

--- static/scripts/__tests__/Search-test.js
+++ static/scripts/__tests__/Search-test.js
@@ -73,4 +73,37 @@ describe('Type ahead', () => {
 
         expect($.ajax.mock.calls.length).toBe(0);
     });
+
+    it('changes selected item with arrow keys', () => {
+        var typeAhead = TestUtils.renderIntoDocument(<TypeAhead/>);
+        var searchInput = TestUtils.findRenderedDOMComponentWithTag(typeAhead, 'input');
+        var typeAheadNode = ReactDOM.findDOMNode(typeAhead);
+        var up = () => {
+            TestUtils.Simulate.keyDown(searchInput, {
+                key: 'ArrowUp', keyCode: 38, which: 38
+            });
+        }
+        var down = () => {
+            TestUtils.Simulate.keyDown(searchInput, {
+                key: 'ArrowDown', keyCode: 40, which: 40
+            });
+        }
+
+        typeAhead.setState({query: '...'});
+        TestUtils.Simulate.change(searchInput);
+
+        expect(typeAhead.state.selected).toBe(0);
+        down();
+        expect(typeAhead.state.selected).toBe(1);
+        up();
+        expect(typeAhead.state.selected).toBe(0);
+        // Now make sure we can't move past the limits
+        up();
+        up();
+        expect(typeAhead.state.selected).toBe(0);
+        down();
+        down();
+        down();
+        expect(typeAhead.state.selected).toBe(typeAhead.state.results.length);
+    });
 });
