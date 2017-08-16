95f1aa16ffee00e4a546ec5d7686cee23f8b02c2
Wil Linssen <wil@linssen.me>
2017-08-16 15:43:27 +0100

    Starting on results

--- static/scripts/components/Result.vue
+++ static/scripts/components/Result.vue
@@ -1,9 +1,58 @@
 <template>
-<p>{{ id }} - {{ slug }}</p>
+
+<div>
+    <h1>{{ winner.title }} wins!</h1>
+    <div className="film-results">
+
+    </div>
+    <div className="info">
+        <hr />
+
+        <h3>Why does this even exist?</h3>
+
+        <p>Because <a href="http://twitter.com/linssen">Wil</a>, <a href="http://twitter.com/gregwood">Greg</a>,
+            and <a href="http://twitter.com/glenswinfield">Glen</a> were in a pub once
+            and they thought you should be able to compare films to Predator. It is
+            after all the ultimate benchmark.</p>
+
+        <router-link :to="{name: 'search'}" class="button button--again info__button">
+            Again!
+        </router-link>
+
+        <a className="button button--tweet info__button info__button--last" target="_blank" v-bind:href="tweetUrl">
+            Tweet this
+        </a>
+
+        <p className="credit">
+            Copyright © {{ year }} <a href="http://linssen.me/">Wil Linssen</a>,
+            and all of the code is <a href="http://github.com/linssen/better_than_predator">on GitHub</a>.<br />
+            Powered by <a href="https://www.themoviedb.org/">themoviedb.org</a>.
+        </p>
+
+    </div>
+</div>
+
 </template>
+
 <script>
 export default {
     name: 'result',
-    props: ['id', 'slug']
+    props: ['id', 'slug'],
+    data: () => {
+        return {
+            year: new Date().getFullYear()
+        }
+    },
+    computed: {
+        films () {
+
+        },
+        winner () {
+            return {title: this.id};
+        },
+        tweetUrl () {
+            return 'TWITTER';
+        }
+    }
 }
 </script>
