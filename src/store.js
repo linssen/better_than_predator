be3be519b1761eb3e5c2e9bd900a2ec38a3c0b5d
Wil Linssen <wil@linssen.me>
2019-01-26 12:17:40 +0000

    Split store to make it easier to test

--- src/store.js
+++ src/store.js
@@ -16,65 +16,71 @@ function mapFilm(item) {
   };
 }
 
+export const actions = {
+  search(context, params) {
+    context.commit('setQuery', { query: params.query });
+    if (!context.state.query || context.state.query.length < 2) {
+      context.commit('clearFilms');
+      return;
+    }
+    context.commit('toggleIsLoading', true);
+    film.find(context.state.query)
+      .then(xhr => context.commit('searchReceived', xhr));
+  },
+  find(context, params) {
+    context.commit('toggleIsLoading', true);
+    film.findOne(params.id)
+      .then(xhr => context.commit('found', xhr));
+  },
+  clearFilms(context) {
+    context.commit('clearFilms');
+  },
+};
+
+export const getters = {
+  filmsByScore(state) {
+    return state.films
+      .slice()
+      .sort((a, b) => parseFloat(b.voteAvg) - parseFloat(a.voteAvg));
+  },
+  poster(state) {
+    return (id) => {
+      const item = state.films.find(f => f.id === id);
+      return `http://image.tmdb.org/t/p/original/${item.posterPath}`;
+    };
+  },
+};
+
+export const mutations = {
+  toggleIsLoading(state, toggle) {
+    if (toggle === null || typeof toggle === 'undefined') {
+      state.isLoading = !state.isLoading;
+    }
+    state.isLoading = toggle;
+  },
+  setQuery(state, props) {
+    state.query = props.query;
+  },
+  clearFilms(state) {
+    state.films = [];
+  },
+  found(state, xhr) {
+    state.isLoading = false;
+    state.films.push(mapFilm(JSON.parse(xhr.responseText)));
+  },
+  searchReceived(state, xhr) {
+    state.isLoading = false;
+    state.films = JSON.parse(xhr.responseText).results.map(mapFilm);
+  },
+};
+
 export default new Vuex.Store({
   state: {
     isLoading: false,
     query: null,
     films: [],
   },
-  actions: {
-    search(context, params) {
-      context.commit('setQuery', { query: params.query });
-      if (!context.state.query || context.state.query.length < 2) {
-        context.commit('clearFilms');
-        return;
-      }
-      context.commit('toggleIsLoading', true);
-      film.find(context.state.query)
-        .then(xhr => context.commit('searchReceived', xhr));
-    },
-    find(context, params) {
-      context.commit('toggleIsLoading', true);
-      film.findOne(params.id)
-        .then(xhr => context.commit('found', xhr));
-    },
-    clearFilms(context) {
-      context.commit('clearFilms');
-    },
-  },
-  mutations: {
-    toggleIsLoading(state, toggle) {
-      if (toggle === null || typeof toggle === 'undefined') {
-        state.isLoading = !state.isLoading;
-      }
-      state.isLoading = toggle;
-    },
-    setQuery(state, props) {
-      state.query = props.query;
-    },
-    clearFilms(state) {
-      state.films = [];
-    },
-    found(state, xhr) {
-      state.isLoading = false;
-      state.films.push(mapFilm(JSON.parse(xhr.responseText)));
-    },
-    searchReceived(state, xhr) {
-      state.isLoading = false;
-      state.films = JSON.parse(xhr.responseText).results.map(mapFilm);
-    },
-  },
-  getters: {
-    filmsByScore(state) {
-      return state.films
-        .slice()
-        .sort((a, b) => parseFloat(b.voteAvg) - parseFloat(a.voteAvg));
-    },
-    poster(state) {
-      return (id) => {
-        const item = state.films.find(f => f.id === id);
-        return `http://image.tmdb.org/t/p/original/${item.posterPath}`;
-      };
-    },
-  },
+  actions,
+  mutations,
+  getters,
 });
