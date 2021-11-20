bbd5527585d57b1850c2702d5517050d8421d040
Wil Linssen <wil@linssen.me>
2021-11-20 18:32:31 +0000

    hash router for external links

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './i18n';
 import App from './App';
 import './assets/tailwind.css';
