8cf9f5a0effe9c03d6d9ccf8749226bcc62d2eb3
Wil Linssen <wil@linssen.me>
2021-11-21 10:26:41 +0000

    Test Predator not delivered to front end

--- src/__tests__/typeahead.test.tsx
+++ src/__tests__/typeahead.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { screen } from '@testing-library/dom';
-import Typeahead from '../components/Typeahead';
-import userEvent from '@testing-library/user-event';
-import { makeSearchMock } from './utils';
 import { BrowserRouter as Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import Typeahead from '../components/Typeahead';
+import { makeApiResultMock } from './utils';
 import filmSearchFixture from './fixtureFilmSearch.json';
 
 describe('Typeahead', () => {
@@ -13,7 +13,7 @@ describe('Typeahead', () => {
 
   beforeEach(() => {
     // setup a DOM element as a render target
-    container = document.createElement("div");
+    container = document.createElement('div');
     document.body.appendChild(container);
   });
 
@@ -24,29 +24,62 @@ describe('Typeahead', () => {
   });
 
   it('searches after 2 character', async () => {
-    const searchMock = makeSearchMock();
+    const searchMock = makeApiResultMock();
     global.fetch = searchMock;
 
     act(() => {
       render(<Router><Typeahead /></Router>, container);
     });
 
-    const input = await screen.getByPlaceholderText('Find a film...')
+    const input = await screen.getByPlaceholderText('Find a film...');
 
     await act(async () => {
       await userEvent.type(input, 'Ho', { delay: 500 });
-    })
+    });
     // Not yet - we want 3 or more characters
     expect(searchMock).not.toHaveBeenCalled();
 
     // Add the rest
     await act(async () => {
-      await userEvent.type(input, 'ney, I', {delay: 500});
+      await userEvent.type(input, 'ney, I', { delay: 500 });
     });
 
     expect(searchMock).toHaveBeenCalled();
     const films = await screen.findAllByRole('link');
     expect(films.length).toEqual(filmSearchFixture.results.length);
-    expect(films[0].textContent).toBe('Honey, I Shrunk the Kids (1989)')
+    expect(films[0].textContent).toBe('Honey, I Shrunk the Kids (1989)');
+  });
+
+  it('excludes Predator itself from the results', async () => {
+    const fakePredator = {
+      ...filmSearchFixture.results[0],
+      title: 'Predator',
+      release_date: '1987-06-12',
+      id: 106,
+    };
+    const searchFixture = {
+      ...filmSearchFixture,
+      results: [
+        ...filmSearchFixture.results,
+        fakePredator,
+      ],
+    };
+    global.fetch = makeApiResultMock(searchFixture);
+
+    act(() => {
+      render(<Router><Typeahead /></Router>, container);
+    });
+
+    const input = await screen.getByPlaceholderText('Find a film...');
+
+    await act(async () => {
+      await userEvent.type(input, 'Predator', { delay: 500 });
+    });
+
+    const films = await screen.findAllByRole('link');
+    const titles = [...films].map((film) => film.textContent);
+    expect(films.length).toEqual(searchFixture.results.length - 1);
+    expect(titles.indexOf('Honey, I Shrunk the Kids (1989)')).toBe(0);
+    expect(titles.indexOf('Predator (1987)')).toBe(-1);
   });
 });
